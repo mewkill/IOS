17aafa6b2fd5ee7c4e717911967c97b4c3b379aa
Monica <moni.va.mv@gmail.com>
2015-07-22 13:50:29 -0500

    cambios
    validaciones y tab

--- biblioteca/Proyecto_Biblioteca/UserRegisterView.h
+++ biblioteca/Proyecto_Biblioteca/UserRegisterView.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface UserRegisterView : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *txtNombre;
+@property (weak, nonatomic) IBOutlet UITextField *txtApellidos;
+@property (weak, nonatomic) IBOutlet UITextField *txtUsuario;
+@property (weak, nonatomic) IBOutlet UITextField *txtPassword;
+@property (weak, nonatomic) IBOutlet UITextField *txtConfirmaPassword;
+@property (weak, nonatomic) IBOutlet UITextField *txtCorreo;
+@property (weak, nonatomic) IBOutlet UITextField *txtTelefono;
+
+- (IBAction)btnRegistrarUsuario:(UIButton *)sender;
 
 @end
--- biblioteca/Proyecto_Biblioteca/UserRegisterView.m
+++ biblioteca/Proyecto_Biblioteca/UserRegisterView.m
@@ -34,4 +34,24 @@
 }
 */
 
+- (IBAction)btnRegistrarUsuario:(UIButton *)sender {
+    if ([_txtNombre.text isEqualToString:@""]||[_txtApellidos.text isEqualToString:@""]||[_txtUsuario.text isEqualToString:@""]||[_txtPassword.text isEqualToString:@""]||[_txtConfirmaPassword.text isEqualToString:@""]||[_txtCorreo.text isEqualToString:@""]||[_txtTelefono.text isEqualToString:@""])
+    {
+        
+        UIAlertView *camposVacios=[[UIAlertView alloc]initWithTitle:@"Error" message:@"Debes completar la informacion" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [camposVacios show];
+        
+    }
+    else{
+        if ([_txtPassword.text isEqualToString:_txtConfirmaPassword.text]) {
+        
+            NSLog(@"Conciden");
+        }
+        else{
+            UIAlertView *errorPassword=[[UIAlertView alloc]initWithTitle:@"Error" message:@"Las contraseñas deben coincidir" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [errorPassword show];
+        }
+    }
+    
+}
 @end
