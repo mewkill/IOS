c4d94be123a5179291b94bc76d696032ce621b4c
Manuel Pelenco <manuelpelenco@gmail.com>
2015-07-22 16:42:14 -0500

    creacion conexión
    se creo una clase para conectar

---
+++ biblioteca/Proyecto_Biblioteca/Connect.h
@@ -0,0 +1,13 @@
+//
+//  Connect.h
+//  Proyecto_Biblioteca
+//
+//  Created by MAC COMPARTIDA4 on 22/07/15.
+//  Copyright (c) 2015 JALOPEZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Connect : NSObject
+
+@end
---
+++ biblioteca/Proyecto_Biblioteca/Connect.m
@@ -0,0 +1,13 @@
+//
+//  Connect.m
+//  Proyecto_Biblioteca
+//
+//  Created by MAC COMPARTIDA4 on 22/07/15.
+//  Copyright (c) 2015 JALOPEZ. All rights reserved.
+//
+
+#import "Connect.h"
+
+@implementation Connect
+
+@end
